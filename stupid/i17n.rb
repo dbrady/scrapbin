a92cbd16445beebaadf578ecdcc13661b8cdb9db
David Brady <dbrady@shinybit.com>
2009-02-19 18:54:48 -0700

    Removed upcase requirement and added examples in comments.

--- stupid/i17n.rb
+++ stupid/i17n.rb
@@ -1,8 +1,13 @@
 # i17n - Intranumeralization library for Ruby
+# 
+# "internationalization".intranumeralize => "i18n"
+# "multilingualization".intranumeralize => "m17n"
+# "localization".intranumeralize => "l10n"
+# "wasteoftimeization".intranumeralize => "w16n"
 class String
   def intranumeralize
     raise ArgumentError.new("String to be intranumeralized must be at least 5 characters long and end in -tion") unless length>=5 && self[-4..-1].downcase == 'tion'
-    "#{self[0..0]}#{self[1..-2].length}#{self[-1..-1]}".upcase
+    "#{self[0..0]}#{self[1..-2].length}#{self[-1..-1]}"
   end
 end
 
