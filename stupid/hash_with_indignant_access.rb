eca241a2abf84ffd2c568126f2fc7f321e2daf4b
David Brady <dbrady@shinybit.com>
2009-05-04 14:09:01 -0600

    Added HashWithIndignantAccess

---
+++ stupid/hash_with_indignant_access.rb
@@ -0,0 +1,7 @@
+class HashWithIndignantAccess < Hash
+  def [](index)
+    unless key? index
+      raise "No seriously, can't you keep your data types straight? " if ((String === index && key?(index.to_sym)) || (Symbol === index && key?(index.to_s)))
+    end
+  end
+end
