f1e30eed794756fd5153bd1808c429718750f30c
David Brady <dbrady@shinybit.com>
2013-07-09 12:36:00 -0600

    wrapped tests in -bash==__FILE__ block
    * So code is usable as a library

--- scraps/hash_by.rb
+++ scraps/hash_by.rb
@@ -10,32 +10,34 @@ module Enumerable
   end
 end
 
-require 'minitest/unit'
-require 'minitest/autorun'
+if $0==__FILE__
+  require 'minitest/unit'
+  require 'minitest/autorun'
 
-class TestHashBy < MiniTest::Unit::TestCase
-  def test_block_version
-    # test block version
-    (1..10).to_a.hash_by {|x| x % 3 }.must_equal( {0=>[3,6,9], 1=>[1,4,7,10], 2=>[2,5,8]} )
-  end
+  class TestHashBy < MiniTest::Unit::TestCase
+    def test_block_version
+      # test block version
+      (1..10).to_a.hash_by {|x| x % 3 }.must_equal( {0=>[3,6,9], 1=>[1,4,7,10], 2=>[2,5,8]} )
+    end
 
-  def rogues
-    [
-     { name: 'Avdi', location: 'PA' },
-     { name: 'Chuck', location: 'UT' },
-     { name: 'Dave', location: 'UT', extra: 'weirdo' },
-     { name: 'James', location: 'OK' },
-     { name: 'Josh', location: 'CA' },
-     { name: 'Katrina', location: 'CO' }
-     ]
-  end
+    def rogues
+      [
+       { name: 'Avdi', location: 'PA' },
+       { name: 'Chuck', location: 'UT' },
+       { name: 'Dave', location: 'UT', extra: 'weirdo' },
+       { name: 'James', location: 'OK' },
+       { name: 'Josh', location: 'CA' },
+       { name: 'Katrina', location: 'CO' }
+      ]
+    end
 
-  def test_id_version
-    rogues.hash_by(:location).keys.must_equal ['PA','UT','OK','CA','CO']
-    rogues.hash_by(:location)['CA'].must_equal [{name: 'Josh', location: 'CA'}]
-  end
+    def test_id_version
+      rogues.hash_by(:location).keys.must_equal ['PA','UT','OK','CA','CO']
+      rogues.hash_by(:location)['CA'].must_equal [{name: 'Josh', location: 'CA'}]
+    end
 
-  def test_symbol_to_proc_version
-    rogues.hash_by(&:size)[3].must_equal [{name: 'Dave', location: 'UT', extra: 'weirdo' }]
+    def test_symbol_to_proc_version
+      rogues.hash_by(&:size)[3].must_equal [{name: 'Dave', location: 'UT', extra: 'weirdo' }]
+    end
   end
 end
