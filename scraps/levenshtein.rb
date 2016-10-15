a15d6dc88ad37f8d4017bb7e4255611d76c5b12b
David Brady <dbrady@covermymeds.com>
2016-10-14 19:04:10 -0600

    Update levenshtein.rb

--- scraps/levenshtein.rb
+++ scraps/levenshtein.rb
@@ -1,33 +1,33 @@
 class String
   # Calculate the "edit distance" between two strings (Levenshtein distance)
   # d(i, j) = min(d(i-1, j)+1, d(i, j-1)+1, d(i-1, j-1) + r(s(i), t(j)))
-  # 
+  #
   # Examples:
   #
   # "foo".edit_distance_to "bar"
   # # => 3
   # # The edit distance is 3, because all 3 characters must be changed
   # to complete this edit.
-  # 
+  #
   # "dave".edit_distance_to "david"
   # # => 2
   # # The edit  distance here is 2, because the e must be changed, and
   # an additional character added.
-  # 
+  #
   # "How many roads must a man walk down".edit_distance_to("before you call him a man?")
-  # 
+  #
   # NOTE! This is an INSANELY recursive function that executes in
   # exponential time. Use it on short strings only, please! Execution
   # time for 1k strings is on the order of 3-8 seconds--and even then
   # only if they are very close. 256b strings are below 1s; short
   # strings are very fast. (Times are on a dual-core 2.5GHz machine
   # with 4GB of RAM)
-  # 
+  #
   # WARNING: For very disparate strings, the exponent grows VERY
   # quickly. For the function:
-  # 
+  #
   # "abcdefghijklm".edit_distance_to("nopqrstuvwxyz")
-  # 
+  #
   # The execution time on my 2.6GHz, dual-core MBP was 68m 25s.
   def edit_distance_to(s)
     if empty?
@@ -43,5 +43,8 @@ class String
   end
 end
 
-#puts "How many roads must a man walkdown".edit_distance_to("before you call him a man?")
-puts "cheese".edit_distance_to("breeze")
+if $0 == __FILE__
+  # Don't run this, it'll need 17 heat deaths (just kidding, it'll blow stack)
+  # puts "How many roads must a man walkdown".edit_distance_to("before you call him a man?")
+  puts "cheese".edit_distance_to("breeze")
+end
