9304c2ec13d79f2ceb63b092c7bd2e7a0e528559
David Brady <dbrady@shinybit.com>
2022-02-11 10:14:25 -0700

    Add data_and_end.rb

---
+++ ruby/data_and_end.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# DATA is a "File" object, you can call .read and .each_line on it, etc.
+puts DATA.read
+
+__END__
+This is a silly little demo of the __END__ and DATA keywords, which I have
+written for myself here because I keep forgetting how they work. Fortunately,
+whenever I google "ruby __DATA__" it knows what I am looking for. (And I am also
+putting __DATA__ in here in case Future Me ever goes grepping in here for that
+particular nonexistent keyword.)
