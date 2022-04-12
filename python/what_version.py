59346242e799181ae7f8c1327327c35b594a5141
David Brady <dbrady@shinybit.com>
2022-04-12 16:44:27 -0600

    Add what_version.py

---
+++ python/what_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Right now (Ac, circa Spring 2022) we're using Python 3.7 everywhere, so we get
+# ordered hashes, but take care not to dip into features from 3.8, 3.9, or 3.10,
+# which is currently the latest. 3.7 is in security-fix-only mode and is
+# scheduled for End of Life in June 2023.
+
+print("3.7, probably 3.7.13? This is scheduled for EOL after June 2023")
