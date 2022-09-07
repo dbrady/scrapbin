7a53093a12ded6ec73ee4e72bc633934dc9f27ba
David Brady <david.brady@acimacredit.com>
2022-09-07 12:28:37 -0600

    Add extra date formats

--- python/strftime.py
+++ python/strftime.py
@@ -1,8 +1,44 @@
 #!/usr/bin/env python
-from datetime import datetime
 
-now = datetime.now()
+# you can use "from datetime import datetime" to eliminate the
+# "datetime.datetime.xyz()" shenanigans
+import datetime
+
+# you can
+
+now = datetime.datetime.now()
+
+print("You can use now.strftime('format'), like so:")
 print(now.strftime("%F %T")) # %F = %Y-%m-%d and %T = %H:%M:%S
+
+print()
+print("Stack Overflow also has this to say:")
+print("https://stackoverflow.com/questions/26455616/how-can-i-create-basic-timestamps-or-dates-python-3-4")
+
+print("You can also use '{:format}'.format(now) in a formatted string, like:")
+print("'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())")
+print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
+print("or")
+print("'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())")
+print('Timestamp: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
+print()
+print("In Python 2 style you just pass it to %s")
+print("'Date now: %s' % datetime.datetime.now()")
+print('Date now: %s' % datetime.datetime.now())
+
+print("'Date today: %s' % datetime.date.today()")
+print(f"By the way, this is line 32 which should show up as {__LINE__}")
+print('Date today: %s' % datetime.date.today())
+
+today = datetime.date.today()
+print("Today's date is {:%b, %d %Y}".format(today))
+
+schedule = '{:%b, %d %Y}'.format(today) + ' - 6 PM to 10 PM Pacific'
+schedule2 = '{:%B, %d %Y}'.format(today) + ' - 1 PM to 6 PM Central'
+print('Maintenance: %s' % schedule)
+print('Maintenance: %s' % schedule2)
 
 
 # in data_services you may see system.get_time(...); this is a custom method we
