1136a1a2ea319213afd10937a8fabf790fb44f8c
David Brady <david.brady@acimacredit.com>
2022-09-21 09:10:55 -0600

    Strip optimist quick down to something quicker

--- ruby/optimist-quick.rb
+++ ruby/optimist-quick.rb
@@ -3,15 +3,6 @@ require 'optimist'
 
 # Parse options
 opts = Optimist.options do
-  version "#{File.basename(__FILE__)} 1.0.0 (C) 2010 David Brady"
-  banner <<-EOS
-This script is a Optimist examplar, because I can never remember which options and crap I can include. More examples at http://optimist.rubyforge.org/
-
-Usage:
-       #{File.basename(__FILE__)} [options] <target> [<extra_args>]
-where [options] are:
-EOS
-
   opt :occupation, "Occupation", type: :string, default: 'haberdasher'
   opt :cpus, "CPUs", type: :integer, default: 0
   opt :dynos, "Dynowidget Factor", type: :float, default: 1.0
