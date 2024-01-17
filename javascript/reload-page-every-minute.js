3f03e098f77bf8263a96f31a3ac9734f1ff03751
David Brady <dbrady@shinybit.com>
2024-01-16 23:38:25 -0700

    Add javascript

---
+++ javascript/reload-page-every-minute.js
@@ -0,0 +1,5 @@
+// Open the webpage you want to refresh and open the developer console, and then run
+setInterval(function() { location.reload(); }, 60000);
+
+// or
+setInterval( () => location.reload(), 60000);
